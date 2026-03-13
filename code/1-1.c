1889abf20ce888b95c0220ea725aa1a39afea832
jh12031 <jh0100c@gmail.com>
2026-03-13 09:25:57 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int var1 = 100;
+    printf("int : %s\n", var1);
+    printf("hex : %s\n", var1);
+    printf("oct : %s\n", var1);
 
+    char var2 = 'c';
+    printf("char : %s\n", var2);
+
+    char* var3 = "hello world!";
+    printf("string : %s\n", var3);
+
+    float var4 = 3.141592;
+    printf("float : %s\n", var4);
+    printf("exp : %s\n", var4);
+    
     return 0;
 }
