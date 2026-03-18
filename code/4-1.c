1f0ca75b355a0b93111ae9d0712a8e6428b4ccdd
jh12031 <jh0100c@gmail.com>
2026-03-18 18:19:45 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a, b;
+    char op;
+
+    if (scanf("%d %d %c", &a, &b, &op) != 3) return 0;
+
+    if (op == '+') printf("%d\n", a + b);
+    else if (op == '-') printf("%d\n", a - b);
+    else if (op == '*') printf("%d\n", a * b);
+    else if (op == '/') {
+        if (b != 0) printf("%d\n", a / b);
+    }
 
     return 0;
 }
-
