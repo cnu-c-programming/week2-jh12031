45851d1c2836bfa7d94898d679ec4b7806d8a095
jh12031 <jh0100c@gmail.com>
2026-03-19 15:20:03 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int n, primeCheck = 1;
+
+    scanf("%d", &n);
+
+    if (n < 2) {
+        primeCheck = 0;
+    } else {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                primeCheck = 0;
+            }
+        }
+    }
+
+    if (primeCheck) printf("true\n");
+    else printf("false\n");
 
     return 0;
 }
-
